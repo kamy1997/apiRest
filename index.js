8b9fa1fd181bd2128be8b550fc0675b1f81af0fa
kamy1997 <kamykechir@gmail.com>
2022-05-12 16:16:21 +0200

    make it better 3

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose =require('mongoose');
 const playerRoutes = require('./src/routes/api');
 const app=express();
+const cors= require("cors");
+
+
 require("dotenv").config();
 mongoose.connect(process.env.DB_URI,
   { useNewUrlParser: true,
@@ -10,6 +13,7 @@ mongoose.connect(process.env.DB_URI,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/player',playerRoutes);
  app.get('/api',(req,res)=> res.send('api working'));
  
